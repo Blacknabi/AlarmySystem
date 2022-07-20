cb3bb9c92b07aa215fd93e578e602d8be4e63735
Jean Roos Jehu <jeanroosjehu@gmail.com>
2022-07-19 23:48:15 -0400

    Ajustement kek fonksyon. Jesyon ere a k pral antame la a ...Sinon kod nou fonksyonel

--- mainp.py
+++ mainp.py
@@ -16,58 +16,70 @@ def aficheMeni():
     print('|                  Tape K pou kite                   |')
     print('|____________________________________________________|')
     print('')
-    
-def rekipereAntre():
     lisAntre = ['R', 'F', 'A', 'K']
     print('CHWAZI YON OPSYON ')
     antre = input(":::>>> ")
-    while antre.upper() not in lisAntre:
-        print('SVP FE YON BON CHWA ')
+    antre = antre.upper()
+    while antre not in lisAntre:
+        print('SVP FE YON BON CHWA. CHWAZI YON OPSYON ')
         antre = input("::>>> ")
     return antre
-
-
+   
 
 def redemare():
+    print('|----------------------------------------------------|')
+    print('|           ::: PWOGRAME YON REDEMARAJ :::           |')
+    print('|----------------------------------------------------|')
+    ss = input("Antre nan konbyen segonn poun redemare. Eg: <34> : ")
     if platform.system()=='Windows':
-        print(f'Machin ou an pral redemare nan kek segonn.')
-        os.system(f"shutdown /r /t 1")
+        print(f'Machin ou an pral redemare nan {ss} segonn.')
+        os.system(f"shutdown /r /t {ss}")
     else:
-        print(f'Machin ou an pral redemare nan kek segonn.')
-        os.system(f"shutdown /r now")
+        print(f'Machin ou an pral redemare nan {ss} segonn.')
+        os.system(f"shutdown /r /t {ss}")
+   
+    
     
 def femen():
+    print('|----------------------------------------------------|')
+    print('|         ::: PWOGRAME YON ARE OTAMATIK :::          |')
+    print('|----------------------------------------------------|')
+    ss = input("Antre nan konbyen segonn femen. Eg: <34> : ")
     if platform.system()=='Windows':
-        print(f'Machin ou an pral femen.')
-        os.system(f"shutdown /s /t 1")
+        print(f'Machin ou an pral femen nan {ss} segonn.')
+        os.system(f"shutdown /s /t {ss}")
     else:
-        print(f'Machin ou an pral femen.')
-        os.system(f"shutdown /h now")
-        
-        
-    
-    
-
-def alarm(set_alarm_timer):
+        print(f'Machin ou an pral femen nan {ss} segonn.')
+        os.system(f"shutdown /h {ss}")
+           
+                   
+def alarm(ajiste_alam):
     while True:
-        time.sleep(1)
-        current_time = datetime.datetime.now()
-        now = current_time.strftime("%H:%M")
-        date = current_time.strftime("%d/%m/%Y")
-        print("The Set Date is:",date)
+        time.sleep(1.2)
+        lh_kounyea = datetime.datetime.now()
+        now = lh_kounyea.strftime("%H:%M")
+        date = lh_kounyea.strftime("%d/%m/%Y")
+        print("Date jodi a se : ",date)
         print(now)
-        if now == set_alarm_timer:
-            print("Time to Wake up")
+        if now == ajiste_alam:
+            print("Alam lanse...")
             playsound.playsound(son)
             break
 
-def actual_time():
-    global h, mn, son 
-    lh = input("Antre lh a nan foma HH:MM : ")
+
+
+def done_alam():
+    global h, mn, son, foma
+    print('|----------------------------------------------------|')
+    print('|              ::: PWOGRAME YON ALAM :::             |')
+    print('|----------------------------------------------------|')
+    print("|                    Itilize foma  24H               |")
+    print('|----------------------------------------------------|')
+    lh = input("Antre lh a nan foma HH:MM. Eg: <22:15> : ")
     son = input("Antre chemen mizik ou vle a. Eg: <C:/Users/JRJC/Music/aaa.m4a>")
     h, mn = lh.split(":")
-    set_alarm_timer = f"{h}:{mn}"
-    alarm(set_alarm_timer)
+    ajiste_alam = f"{h}:{mn}"
+    alarm(ajiste_alam)
 
 
 
@@ -76,4 +88,16 @@ def kite():
     sys.exit("BYE BYE !")
 
 
-actual_time()
+#---------------------------MAIN---------------------------
+
+
+antre = aficheMeni()
+
+if antre == 'R':
+    redemare()
+elif antre == 'F':
+    femen()
+elif antre == 'A':
+    done_alam()
+else:
+    kite()
